09bd98ce1de943a2c41fc36ff839408427f2aef4
VeenaJeswani <vejes106@gmail.com>
2024-08-30 14:17:07 +0530

    change

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,11 @@ public class HelloWorld {
 static void method(){
   System.out.prinltln("Hello java method");
   System.out.println("Hello changed java method");
+ System.out.println("This is the change");
+
 }
+
+
   public static void main(String[] args){
     System.out.println("hello java world");
     method();
