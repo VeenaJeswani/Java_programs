4d3a14e9fc3ae873ceecf81d14a0f53ae9fb3ad4
VeenaJeswani <vejes106@gmail.com>
2024-08-30 14:36:13 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,10 +4,11 @@ static void method(){
   System.out.println("Hello changed java method");
  System.out.println("This is the change");
 
-}
+
 }
 
   public static void main(String[] args){
     System.out.println("hello java world");
     method();
   }
+}
